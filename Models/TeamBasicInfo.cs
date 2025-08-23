94226ea58d101d9d33c46be9dcf3d7edc43e0315
iLeandros <49075652+iLeandros@users.noreply.github.com>
2025-08-23 23:59:07 +0200

    Update TeamBasicInfo.cs

--- Models/TeamBasicInfo.cs
+++ Models/TeamBasicInfo.cs
@@ -8,56 +8,10 @@ using System.Threading.Tasks;
 
 namespace DataSvc.Models;
 
-public class TeamBasicInfo : INotifyPropertyChanged
+public class TeamBasicInfo
 {
-    private string teamName;
-    private string teamFlag;
-    private string country;
-    private string countryFlag;
-
-    public string TeamName
-    {
-        get => teamName;
-        set
-        {
-            teamName = value;
-            OnPropertyChanged(nameof(TeamName));
-        }
-    }
-
-    public string TeamFlag
-    {
-        get => teamFlag;
-        set
-        {
-            teamFlag = value;
-            OnPropertyChanged(nameof(TeamFlag));
-        }
-    }
-
-    public string Country
-    {
-        get => country;
-        set
-        {
-            country = value;
-            OnPropertyChanged(nameof(Country));
-        }
-    }
-
-    public string CountryFlag
-    {
-        get => countryFlag;
-        set
-        {
-            countryFlag = value;
-            OnPropertyChanged(nameof(CountryFlag));
-        }
-    }
-
-    public event PropertyChangedEventHandler PropertyChanged;
-    protected virtual void OnPropertyChanged(string propertyName)
-    {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-    }
+    public string? TeamName { get; set; }
+    public string? TeamFlag { get; set; }
+    public string? Country { get; set; }
+    public string? CountryFlag { get; set; }
 }
