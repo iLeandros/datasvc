d1349d7a2db058c082dda648fd0af14c6217cf99
iLeandros <49075652+iLeandros@users.noreply.github.com>
2025-08-23 23:48:50 +0200

    Update TeamBasicInfo.cs

--- Models/TeamBasicInfo.cs
+++ Models/TeamBasicInfo.cs
@@ -1,1 +1,57 @@
+using System.Text.Json.Serialization;
 
+namespace DataSvc.Models;
+
+public class TeamBasicInfo : INotifyPropertyChanged
+{
+    private string teamName;
+    private string teamFlag;
+    private string country;
+    private string countryFlag;
+
+    public string TeamName
+    {
+        get => teamName;
+        set
+        {
+            teamName = value;
+            OnPropertyChanged(nameof(TeamName));
+        }
+    }
+
+    public string TeamFlag
+    {
+        get => teamFlag;
+        set
+        {
+            teamFlag = value;
+            OnPropertyChanged(nameof(TeamFlag));
+        }
+    }
+
+    public string Country
+    {
+        get => country;
+        set
+        {
+            country = value;
+            OnPropertyChanged(nameof(Country));
+        }
+    }
+
+    public string CountryFlag
+    {
+        get => countryFlag;
+        set
+        {
+            countryFlag = value;
+            OnPropertyChanged(nameof(CountryFlag));
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
