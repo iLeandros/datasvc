96478612024ae9b108b1143ea1843f4ffa9cd7d2
iLeandros <49075652+iLeandros@users.noreply.github.com>
2025-08-24 00:56:27 +0200

    Update renameTeam.cs

--- ModelHelperCalls/renameTeam.cs
+++ ModelHelperCalls/renameTeam.cs
@@ -1,1 +1,16 @@
+namespace DataSvc.ModelHelperCalls;
 
+public class renameTeam
+{
+    public static string renameTeamNameToFitDisplayLabel(string team)
+    {
+        if (team.Length > 13)
+        {
+            return team.Substring(0, 13) + "."; ;
+        }
+        else
+        {
+            return team;
+        }
+    }
+}
