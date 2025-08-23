71668ac63292579a279783c9e8c18cc779a454eb
iLeandros <49075652+iLeandros@users.noreply.github.com>
2025-08-24 00:44:31 +0200

    Update DetailsTableDataItemMatchesBetween.cs

--- Models/DetailsTableDataItemMatchesBetween.cs
+++ Models/DetailsTableDataItemMatchesBetween.cs
@@ -1,1 +1,10 @@
+namespace DataSvc.Models;
 
+    public DetailsTableDataItemMatchesBetween(Match match, Details details)
+    {
+        this.sMatch = match;
+        this.sDetails = details;
+    }
+    public Match sMatch { get; set; }
+    public Details sDetails { get; set; }
+}
