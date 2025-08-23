56c53f1f0497afdbbeb5a425258027542c4f7801
iLeandros <49075652+iLeandros@users.noreply.github.com>
2025-08-24 00:54:59 +0200

    Update DetailsTableDataGroupMatchesBetween.cs

--- Models/DetailsTableDataGroupMatchesBetween.cs
+++ Models/DetailsTableDataGroupMatchesBetween.cs
@@ -1,1 +1,12 @@
+using System.Collections.ObjectModel;
 
+namespace DataSvc.Models;
+
+public class DetailsTableDataGroupMatchesBetween : ObservableCollection<DetailsTableDataItemMatchesBetween>
+{
+    public DetailsTableDataGroupMatchesBetween(ObservableCollection<DetailsTableDataItemMatchesBetween> animals) : base(animals)
+    {
+        this.Items = new ObservableCollection<DetailsTableDataItemMatchesBetween>();
+    }
+    public new ObservableCollection<DetailsTableDataItemMatchesBetween> Items { get; set; }
+}
