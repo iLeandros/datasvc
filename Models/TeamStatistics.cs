6412045d4aebf9d341e7c4616452ca421bd44ed2
iLeandros <49075652+iLeandros@users.noreply.github.com>
2025-08-24 00:38:13 +0200

    Update TeamStatistics.cs

--- Models/TeamStatistics.cs
+++ Models/TeamStatistics.cs
@@ -1,1 +1,8 @@
+namespace DataSvc.Models;
 
+public class TeamStatistics
+{
+    public string TeamName { get; set; }
+    public string TeamLogoUrl { get; set; }
+    public List<FactItem> FactItems { get; set; }
+}
