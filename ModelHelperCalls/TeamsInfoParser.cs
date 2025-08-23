39788df238d3b38952184f187edc885d9f12587d
iLeandros <49075652+iLeandros@users.noreply.github.com>
2025-08-23 23:51:33 +0200

    Update TeamsInfoParser.cs

--- ModelHelperCalls/TeamsInfoParser.cs
+++ ModelHelperCalls/TeamsInfoParser.cs
@@ -1,1 +1,58 @@
+using System.Collections.ObjectModel;
+using System.Text.Json.Serialization;
 
+namespace DataSvc.ModelHelperCalls;
+
+public class TeamBasicInfo : INotifyPropertyChanged
+{
+    private string teamName;
+    private string teamFlag;
+    private string country;
+    private string countryFlag;
+
+    public string TeamName
+    {
+        get => teamName;
+        set
+        {
+            teamName = value;
+            OnPropertyChanged(nameof(TeamName));
+        }
+    }
+
+    public string TeamFlag
+    {
+        get => teamFlag;
+        set
+        {
+            teamFlag = value;
+            OnPropertyChanged(nameof(TeamFlag));
+        }
+    }
+
+    public string Country
+    {
+        get => country;
+        set
+        {
+            country = value;
+            OnPropertyChanged(nameof(Country));
+        }
+    }
+
+    public string CountryFlag
+    {
+        get => countryFlag;
+        set
+        {
+            countryFlag = value;
+            OnPropertyChanged(nameof(CountryFlag));
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
