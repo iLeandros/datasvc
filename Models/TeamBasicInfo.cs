a7d58cf8890ca412da7106925ece6d91d968285a
iLeandros <49075652+iLeandros@users.noreply.github.com>
2025-08-24 00:39:42 +0200

    Update TeamBasicInfo.cs

--- Models/TeamBasicInfo.cs
+++ Models/TeamBasicInfo.cs
@@ -1,11 +1,3 @@
-using System.Text.Json.Serialization;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace DataSvc.Models;
 
 public class TeamBasicInfo
