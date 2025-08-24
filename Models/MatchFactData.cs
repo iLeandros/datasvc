4a0067e3cb5f9e16ec5f4e1bc1f82bcf2f2db81c
iLeandros <49075652+iLeandros@users.noreply.github.com>
2025-08-24 14:57:19 +0200

    Update MatchFactData.cs

--- Models/MatchFactData.cs
+++ Models/MatchFactData.cs
@@ -1,1 +1,9 @@
+using System.Collections.ObjectModel;
 
+public class MatchFactData
+{
+    public string Name { get; set; }
+    public int Percentage { get; set; }
+    // Add a property for the normalized percentage
+    public double NormalizedPercentage => Percentage / 100.0;
+}
