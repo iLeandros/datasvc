a181028a4a8dbca5ad6a7d4579114091b8c3b0bc
iLeandros <49075652+iLeandros@users.noreply.github.com>
2025-08-24 04:24:26 +0200

    Update MatchItem.cs

--- Models/MatchItem.cs
+++ Models/MatchItem.cs
@@ -1,1 +1,12 @@
+namespace DataSvc.Models;
 
+public class MatchItem
+{
+    public string Competition { get; set; }
+    public string Date { get; set; }
+    public string HostTeam { get; set; }
+    public int HostGoals { get; set; }
+    public string GuestTeam { get; set; }
+    public int GuestGoals { get; set; }
+    public List<MatchAction> Actions { get; set; } = new List<MatchAction>();
+}
