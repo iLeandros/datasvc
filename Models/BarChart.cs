764870fee24449beb28edc5517bf47c0785b6654
iLeandros <49075652+iLeandros@users.noreply.github.com>
2025-08-24 14:56:40 +0200

    Update BarChart.cs

--- Models/BarChart.cs
+++ Models/BarChart.cs
@@ -1,1 +1,16 @@
+using System.Collections.ObjectModel;
 
+namespace DataSvc.Models;
+
+public class BarChart : ObservableCollection<MatchFactData>
+{
+    public string Title { get; set; }
+    public string HalfContainerId { get; set; }
+    public List<MatchFactData> MatchFacts { get; set; }
+
+    public BarChart(string title, List<MatchFactData> matchFacts) : base(matchFacts)
+    {
+        Title = title;
+        MatchFacts = matchFacts;
+    }
+}
