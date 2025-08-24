dd7ae8940add1dfad2542aed82a9b4814fad860c
iLeandros <49075652+iLeandros@users.noreply.github.com>
2025-08-24 04:26:53 +0200

    Update MatchData.cs

--- Models/MatchData.cs
+++ Models/MatchData.cs
@@ -1,1 +1,8 @@
+using System.Collections.ObjectModel;
 
+namespace DataSvc.Models;
+
+public class MatchData
+{
+    public ObservableCollection<MatchItem> Matches { get; set; } = new ObservableCollection<MatchItem>();
+}
