2ec239af0febba2586ced96f9a353dc633ce1ac4
iLeandros <49075652+iLeandros@users.noreply.github.com>
2025-08-24 04:25:55 +0200

    Update MatchAction.cs

--- Models/MatchAction.cs
+++ Models/MatchAction.cs
@@ -1,1 +1,9 @@
+namespace DataSvc.Models;
 
+public class MatchAction
+{
+    public string TeamType { get; set; } // "hostteam" or "guestteam"
+    public string ActionType { get; set; } // e.g., "goal", "ycard", "penalty", etc.
+    public string Player { get; set; }
+    public string Time { get; set; } // e.g., "46' Pogba Paul"
+}
