9eccee6607f5f5fb58963c6d200d29a135e58e83
iLeandros <49075652+iLeandros@users.noreply.github.com>
2025-08-25 01:22:05 +0200

    Update TeamStanding.cs

--- Models/TeamStanding.cs
+++ Models/TeamStanding.cs
@@ -1,1 +1,14 @@
-
+public class TeamStanding
+{
+    public int Position { get; set; }
+    public string TeamName { get; set; }
+    public string TeamUrl { get; set; }
+    public int Matches { get; set; }
+    public int Points { get; set; }
+    public Record Overall { get; set; }
+    public Record Home { get; set; }
+    public Record Away { get; set; }
+    public bool IsHighlite { get; set; } // New property
+    public bool IsMark { get; set; } // Indicates if the row is highlighted with "mark"
+    public bool IsHeader { get; set; } // Indicates if the row is highlighted with "mark"
+}
