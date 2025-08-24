63a51450f0615b1865ab55bf8ac2f3024bbeb1c4
iLeandros <49075652+iLeandros@users.noreply.github.com>
2025-08-25 01:39:53 +0200

    Update Record.cs

--- Models/Record.cs
+++ Models/Record.cs
@@ -1,1 +1,11 @@
+namespace DataSvc.Models;
 
+public class Record
+{
+    public int Wins { get; set; }
+    public int Draws { get; set; }
+    public int Losses { get; set; }
+    public int GoalsScored { get; set; }
+    public int GoalsConceded { get; set; }
+    public int GoalDifference { get; set; }
+}
