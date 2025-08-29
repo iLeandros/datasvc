6bb720fc994a047c729846119d444bc4b260148b
iLeandros <49075652+iLeandros@users.noreply.github.com>
2025-08-29 02:52:49 +0200

    Update TableDataGroup.cs

--- Models/TableDataGroup.cs
+++ Models/TableDataGroup.cs
@@ -3,20 +3,22 @@ using System.Text.Json.Serialization;
 
 namespace DataSvc.Models;
 
-public class TableDataGroup : ObservableCollection<TableDataItem>
+public class TableDataGroup
 {
-    [JsonPropertyName("ImagePath")] public string? ImagePath { get; set; }
+    [JsonPropertyName("ImagePath")]     public string? ImagePath     { get; set; }
     [JsonPropertyName("Championship")]  public string? Championship  { get; set; }
-    [JsonPropertyName("Tip")]   public string? Tip   { get; set; }
+    [JsonPropertyName("Tip")]           public string? Tip           { get; set; }
+
+    // If you want the JSON key to be exactly "Items" (not "items"), keep the attribute:
+    [JsonPropertyName("Items")] public ObservableCollection<TableDataItem> Items { get; set; } = new();
 
     public TableDataGroup() { }
 
     public TableDataGroup(string groupImage, string groupName, string tipLabel, ObservableCollection<TableDataItem> items)
-        : base(items)
     {
         ImagePath = groupImage;
-        Championship  = groupName;
-        Tip   = tipLabel;
+        Championship = groupName;
+        Tip = tipLabel;
+        Items = items ?? new ObservableCollection<TableDataItem>();
     }
 }
-
