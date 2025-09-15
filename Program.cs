a6acbe342e299d9fd9aab5d712a00fc6f1c58e61
iLeandros <49075652+iLeandros@users.noreply.github.com>
2025-09-16 01:12:22 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1451,7 +1451,7 @@ public static class GetStartupMainTableDataGroup2024
                                 var tipClass = tip.Attributes["class"].Value;
                                 if (tipClass == "value success") backgroundtipcolor = Colors.Green;
                                 else if (tipClass == "value failed") backgroundtipcolor = Colors.Red;
-                                else backgroundtipcolor = Colors.DarkSlateGray;
+                                else backgroundtipcolor = Colors.Black;
                             }
 
                             var likebutton = matchItem.Descendants("div")
