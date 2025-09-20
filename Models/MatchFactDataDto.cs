5ad5d4b2f6d8e3c82ee387a341ea4c527db987c5
iLeandros <49075652+iLeandros@users.noreply.github.com>
2025-09-20 03:41:31 +0200

    Update MatchFactDataDto.cs

--- Models/MatchFactDataDto.cs
+++ Models/MatchFactDataDto.cs
@@ -1,1 +1,9 @@
+namespace DataSvc.Models;
 
+public sealed class MatchFactDataDto
+{
+    // server sends PascalCase here (from your model), but deserializer is case-insensitive
+    public string? Name { get; set; }
+    public int Percentage { get; set; }
+    public double NormalizedPercentage { get; set; }
+}
