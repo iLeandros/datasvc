999d1898efdadba0f109b1f64b0f4d620693475e
iLeandros <49075652+iLeandros@users.noreply.github.com>
2025-09-20 03:40:31 +0200

    Update BarChartDto.cs

--- Models/BarChartDto.cs
+++ Models/BarChartDto.cs
@@ -1,1 +1,8 @@
+namespace DataSvc.Models;
 
+public sealed class BarChartDto
+{
+    public string? Title { get; set; }
+    public string? HalfContainerId { get; set; }
+    public List<MatchFactDataDto> Items { get; set; } = new();
+}
