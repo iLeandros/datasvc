5fa35f1e2fbf3bb2fa650f991f64c8e556b5a492
iLeandros <49075652+iLeandros@users.noreply.github.com>
2025-09-20 03:38:13 +0200

    Update MatchBetweenItemDto.cs

--- Models/MatchBetweenItemDto.cs
+++ Models/MatchBetweenItemDto.cs
@@ -1,1 +1,12 @@
+namespace DataSvc.Models;
 
+public sealed class MatchBetweenItemDto
+{
+    public string? Competition { get; set; }
+    public string? Date { get; set; }
+    public string? HostTeam { get; set; }
+    public int HostGoals { get; set; }
+    public string? GuestTeam { get; set; }
+    public int GuestGoals { get; set; }
+    // plus any actions you included
+}
