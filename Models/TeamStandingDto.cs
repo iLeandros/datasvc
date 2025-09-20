6ebb1ad2b49777dc51cbfa627027e379eed2ff8e
iLeandros <49075652+iLeandros@users.noreply.github.com>
2025-09-20 03:47:04 +0200

    Update TeamStandingDto.cs

--- Models/TeamStandingDto.cs
+++ Models/TeamStandingDto.cs
@@ -1,1 +1,15 @@
+namespace DataSvc.Models;
 
+public sealed class TeamStandingDto
+{
+    public int Position { get; set; }
+    public string? TeamName { get; set; }
+    public string? TeamUrl { get; set; }
+    public int Matches { get; set; }
+    public int Points { get; set; }
+    public RecordDto Overall { get; set; } = new();
+    public RecordDto Home { get; set; } = new();
+    public RecordDto Away { get; set; } = new();
+    public bool IsHighlite { get; set; }
+    public bool IsMark { get; set; }
+}
