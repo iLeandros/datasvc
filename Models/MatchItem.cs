fbbb31f829c1a9d954fbbe792ed9dec35a748605
iLeandros <49075652+iLeandros@users.noreply.github.com>
2025-11-05 07:37:55 +0100

    Make Competition and Date properties nullable

--- Models/MatchItem.cs
+++ Models/MatchItem.cs
@@ -2,12 +2,13 @@ namespace DataSvc.Models;
 
 public class MatchItem
 {
-    public string Competition { get; set; }
-    public string Date { get; set; }
+    public string? Competition { get; set; }
+    public string? Date { get; set; }
     public string HostTeam { get; set; }
     public int HostGoals { get; set; }
     public string GuestTeam { get; set; }
     public int GuestGoals { get; set; }
-    public List<MatchAction> Actions { get; set; } = new List<MatchAction>();
+    public List<MatchAction> Actions { get; set; } = new();
     public HalfTimeScore HalfTime { get; set; } = new(0,0);
+
 }
