583a52c07dd2a0aadc10172f999abdfa7f8bdc61
iLeandros <49075652+iLeandros@users.noreply.github.com>
2025-11-10 07:52:32 +0100

    Update LiveScoresParser.cs

--- ModelHelperCalls/LiveScoresParser.cs
+++ ModelHelperCalls/LiveScoresParser.cs
@@ -203,7 +203,7 @@ public static class LiveScoresParser
             }
     
             // ---- remove or keep your debug lines as you like ----
-            // actionsList.Add(new MatchAction(TeamSide.Host, ActionKind.Unknown, actionNodes.Count, "furk"));
+            actionsList.Add(new MatchAction(TeamSide.Host, ActionKind.Unknown, actionNodes.Count, matchId));
             // actionsList.Add(new MatchAction(TeamSide.Host, ActionKind.Unknown, matchNodes.Count, actionsRoot?.InnerHtml ?? ""));
     
             list.Add(new LiveScoreItem(
