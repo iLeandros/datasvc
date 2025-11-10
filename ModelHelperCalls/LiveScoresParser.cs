26cbbf7d649512f9f4e4c21d04d7c220f31402c2
iLeandros <49075652+iLeandros@users.noreply.github.com>
2025-11-10 08:36:39 +0100

    Update LiveScoresParser.cs

--- ModelHelperCalls/LiveScoresParser.cs
+++ ModelHelperCalls/LiveScoresParser.cs
@@ -154,7 +154,7 @@ public static class LiveScoresParser
     
                 actionsList.Add(new MatchAction(side, kind, minute, player));
             }
-            */
+            
             // ---- 2) NEW: if no actions yet, hit the Ajax endpoint and parse that snippet ----
             actionsList.Add(new MatchAction(TeamSide.Host, ActionKind.Unknown, 1111, "ARIS"));
             try
@@ -202,9 +202,9 @@ public static class LiveScoresParser
                 }
             
             // ---- remove or keep your debug lines as you like ----
-            actionsList.Add(new MatchAction(TeamSide.Host, ActionKind.Unknown, 11, matchId));
+            //actionsList.Add(new MatchAction(TeamSide.Host, ActionKind.Unknown, 11, matchId));
             // actionsList.Add(new MatchAction(TeamSide.Host, ActionKind.Unknown, matchNodes.Count, actionsRoot?.InnerHtml ?? ""));
-    
+            */
             list.Add(new LiveScoreItem(
                 time,
                 status,
@@ -212,7 +212,8 @@ public static class LiveScoresParser
                 homeGoals,
                 awayGoals,
                 awayName,
-                actionsList
+                actionsList,
+                matchId
             ));
         }
     
