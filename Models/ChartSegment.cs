9cb75afdcbb7714447638bae450f34594b363f67
iLeandros <49075652+iLeandros@users.noreply.github.com>
2025-11-12 02:21:15 +0100

    Update ChartSegment.cs

--- Models/ChartSegment.cs
+++ Models/ChartSegment.cs
@@ -1,1 +1,10 @@
+using System.Collections.ObjectModel;
 
+namespace DataSvc.Models;
+
+public class ChartSegment
+{
+    public double Percentage { get; set; }
+    public string Color { get; set; }
+    public string Text { get; set; }
+}
