145b27068555e33c2fcef96f14f3a8d082f6afeb
iLeandros <49075652+iLeandros@users.noreply.github.com>
2025-11-12 03:37:51 +0100

    Create LiveTableDataGroupDto.cs

---
+++ Models/LiveTableDataGroupDto.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace DataSvc.Models;
+
+public class LiveTableDataGroupDto : ObservableCollection<LiveTableDataItemDto>
+{
+    public string Title { get; }
+    public Color HeaderColor { get; }
+
+    public LiveTableDataGroupDto(Color headerColor, string title, ObservableCollection<LiveTableDataItemDto> items)
+        : base(items ?? new ObservableCollection<LiveTableDataItemDto>())
+    {
+        HeaderColor = headerColor;
+        Title = title;
+    }
+
+    // Optional: total matches for header
+    public int CountLabel => this?.Count ?? 0;
+}
