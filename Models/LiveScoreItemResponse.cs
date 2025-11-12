72f17cc117349aaad6e33249586bd1703a211035
iLeandros <49075652+iLeandros@users.noreply.github.com>
2025-11-12 03:42:00 +0100

    Create LiveScoreItemResponse.cs

---
+++ Models/LiveScoreItemResponse.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataSvc.Models;
+
+public class LiveScoreItemResponse
+{
+    public string Time { get; set; }
+    public string Status { get; set; }
+    public string HomeTeam { get; set; }
+    public string HomeGoals { get; set; }
+    public string AwayGoals { get; set; }
+    public string AwayTeam { get; set; }
+    public List<MatchAction> Action { get; set; }
+    public string MatchID { get; set; }
+}
