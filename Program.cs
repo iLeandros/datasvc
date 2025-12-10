11c71727fc1ca5297289510c596c6001a5e94584
iLeandros <49075652+iLeandros@users.noreply.github.com>
2025-12-10 01:49:27 +0100

    Replace OldCommentsCleanupJob with OldDataCleanupJob

--- Program.cs
+++ Program.cs
@@ -115,7 +115,7 @@ builder.Services.AddHostedService<PerDateRefreshJob>();
 builder.Services.AddSingleton<ParsedTipsService>();
 //builder.Services.AddHostedService<ParsedTipsRefreshJob>();
 
-builder.Services.AddHostedService<OldCommentsCleanupJob>();
+builder.Services.AddHostedService<OldDataCleanupJob >();
 
 // Program.cs (server)
 builder.Services.ConfigureHttpJsonOptions(o =>
