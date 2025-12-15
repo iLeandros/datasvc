f0b78129104f805c80fcc6f2626bbf57fd72b375
iLeandros <49075652+iLeandros@users.noreply.github.com>
2025-12-15 01:54:10 +0100

    Rename DetailsDto to DetailsItemDto

--- Models/TableDataItem.cs
+++ Models/TableDataItem.cs
@@ -44,7 +44,8 @@ public class TableDataItem
     public bool TipIsVisible { get; set; } = true;
     public string BackgroundColor { get; set; } = "#D3D3D3"; // LightGray
     //public Microsoft.Maui.Graphics.Color BackgroundColor { get; set; } = Microsoft.Maui.Graphics.Colors.LightGray // Default to LightGray
-    public DetailsItemDto DetailsDto { get; set; }
+    //public DetailsItemDto DetailsDto { get; set; }
+    public DetailsItemDto DetailsItemDto { get; set; }
     
 
     public TableDataItem() { }
