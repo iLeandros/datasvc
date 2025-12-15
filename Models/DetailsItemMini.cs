c71e14950b9813749549fcd655994a7927870482
iLeandros <49075652+iLeandros@users.noreply.github.com>
2025-12-15 01:36:48 +0100

    Refactor MatchDataBetween property in DetailsItemMini

--- Models/DetailsItemMini.cs
+++ Models/DetailsItemMini.cs
@@ -7,12 +7,6 @@ public sealed class DetailsItemMini
     public List<TeamBasicInfoDto>?    TeamsInfo { get; set; }
     public List<MatchFactDto>?        MatchFacts { get; set; }
     public TeamStandingsTableDto?     TeamStandings { get; set; }
-
-    public class H2HDto
-    {
-        public List<H2HMatchDto>? Matches { get; set; }
-    }
-    public H2HDto? MatchDataBetween { get; set; }
-
-    public List<BarChartDto>? BarCharts { get; set; }
+    public MatchDataBetween?          MatchDataBetween { get; set; }
+    public List<BarChartDto>?         BarCharts { get; set; }
 }
