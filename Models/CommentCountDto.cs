7b841b3b513f1fa74179dcd402e10a15eebcdb41
iLeandros <49075652+iLeandros@users.noreply.github.com>
2025-12-23 01:47:31 +0100

    Add CommentCountDto class with properties

--- Models/CommentCountDto.cs
+++ Models/CommentCountDto.cs
@@ -1,1 +1,8 @@
-
+namespace DataSvc.Models;
+public sealed class CommentCountDto
+{
+    public ulong? MatchId { get; set; }
+    public int Total { get; set; }
+    public int TopLevel { get; set; }
+    public int Replies { get; set; }
+}
