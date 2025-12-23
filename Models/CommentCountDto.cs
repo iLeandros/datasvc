8b9faa2ea72df21086c9809a0bbbc52b28632921
iLeandros <49075652+iLeandros@users.noreply.github.com>
2025-12-23 03:36:44 +0100

    Update CommentCountDto.cs

--- Models/CommentCountDto.cs
+++ Models/CommentCountDto.cs
@@ -1,7 +1,7 @@
 namespace DataSvc.Models;
 public sealed class CommentCountDto
 {
-    public ulong? MatchId { get; set; }
+    public string? Href { get; set; }
     public int Total { get; set; }
     public int TopLevel { get; set; }
     public int Replies { get; set; }
