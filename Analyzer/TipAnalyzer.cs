5642be73cbcdfc5e5720ac1ea46b2b437eee3e35
iLeandros <49075652+iLeandros@users.noreply.github.com>
2026-01-02 05:20:20 +0100

    Update TipAnalyzer.cs

--- Analyzer/TipAnalyzer.cs
+++ Analyzer/TipAnalyzer.cs
@@ -528,8 +528,7 @@ public static class TipAnalyzer
         double hto15 = double.NaN, hto25 = double.NaN, hto35 = double.NaN;
         double htu15 = double.NaN, htu25 = double.NaN, htu35 = double.NaN;
         
-        double lamTFull = ((lamHElo is not null) ? lamHElo.Value : 0.0)
-                + ((lamAElo is not null) ? lamAElo.Value : 0.0);
+        double lamTFull = (lamHElo ?? 0.0) + (lamAElo ?? 0.0);
 
         if (double.IsNaN(hto15) && lamTFull > 0)
         {
