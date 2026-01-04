04b19face59a69f02e62490c343ff537db998c80
iLeandros <49075652+iLeandros@users.noreply.github.com>
2026-01-04 05:33:47 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -461,17 +461,10 @@ app.MapGet("/app-ads.txt", async ctx =>
     // cache a bit (adjust as you like)
     //ctx.Response.Headers["Cache-Control"] = "public, max-age=3600";
 	ctx.Response.Headers["Cache-Control"] = "no-store";
-    await ctx.Response.WriteAsync("google.com, pub-3388518972215761, DIRECT, f08c47fec0942fa0, facebook.com, 2656320454390110, DIRECT, c3e20eee3f780d68");
+    //await ctx.Response.WriteAsync("google.com, pub-3388518972215761, DIRECT, f08c47fec0942fa0");
+	await ctx.Response.WriteAsync("facebook.com, 2656320454390110, DIRECT, c3e20eee3f780d68");
 });
 
-app.MapGet("/app-ads2.txt", async ctx =>
-{
-    ctx.Response.ContentType = "text/plain; charset=utf-8";
-    ctx.Response.Headers["Cache-Control"] = "no-store";
-    ctx.Response.Headers["X-AppAds-From"] = "kestrel-2026-01-04";
-
-    await ctx.Response.WriteAsync("TEST-2026-01-04");
-});
 
 
 app.MapGet("/app-version.txt", async ctx =>
