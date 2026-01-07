a5ca5a6317df7361612a53d79aa62f00cf52aad2
iLeandros <49075652+iLeandros@users.noreply.github.com>
2026-01-08 00:37:02 +0100

    Create Endpoints.cs

---
+++ Parsed/Endpoints.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using System.Text.Json;
+
+namespace DataSvc.Parsed;
