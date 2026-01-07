3bbc0cda08cb2befed06e57d4cc64fad1efb1011
iLeandros <49075652+iLeandros@users.noreply.github.com>
2026-01-08 00:39:06 +0100

    Update Endpoints.cs

--- Parsed/Endpoints.cs
+++ Parsed/Endpoints.cs
@@ -1,10 +1,17 @@
-using System.Collections.Generic;
-using System.Linq;
+// Parsed/Endpoints.cs
 using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Text.Json;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
-using System.Text.Json;
+using Microsoft.Extensions.DependencyInjection;
+
 
 namespace DataSvc.Parsed;
+
