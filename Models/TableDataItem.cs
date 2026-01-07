b86b78c4ccabdd392e1ae96c24d5f5bc8d9c9231
iLeandros <49075652+iLeandros@users.noreply.github.com>
2026-01-07 02:06:29 +0100

    Update TableDataItem.cs

--- Models/TableDataItem.cs
+++ Models/TableDataItem.cs
@@ -21,24 +21,24 @@ public class TableDataItem
     public string? VIPTip { get; set; }
     public string VIPTipElo { get; set; }
     [JsonPropertyName("Tip")] public string? Tip { get; set; }
-    [JsonPropertyName("Like")] public string? LikeButtonImage { get; set; }
-    [JsonPropertyName("Dislike")] public string? DislikeButtonImage { get; set; }
-    [JsonPropertyName("Likes")] public string? LikePositive { get; set; }
-    [JsonPropertyName("Dislikes")] public string? LikeNegative { get; set; }
+    //[JsonPropertyName("Like")] public string? LikeButtonImage { get; set; }
+    //[JsonPropertyName("Dislike")] public string? DislikeButtonImage { get; set; }
+    //[JsonPropertyName("Likes")] public string? LikePositive { get; set; }
+    //[JsonPropertyName("Dislikes")] public string? LikeNegative { get; set; }
     public long ServerComputedLikes { get; set; }           // numeric
     public string ServerComputedLikesFormatted { get; set; } // ready for UI
-    public string? One { get; set; }
-    public string? Even { get; set; }
-    public string? Two { get; set; }
-    public string? HalfOne { get; set; }
-    public string? HalfEven { get; set; }
-    public string? HalfTwo { get; set; }
-    public string? OneAbove { get; set; }
-    public string? TwoAbove { get; set; }
-    public string? ThreeAbove { get; set; }
-    public string? BTS { get; set; }
-    public string? OTS { get; set; }
-    public string? YourPrediction { get; set; }
+    //public string? One { get; set; }
+    //public string? Even { get; set; }
+    //public string? Two { get; set; }
+    //public string? HalfOne { get; set; }
+    //public string? HalfEven { get; set; }
+    //public string? HalfTwo { get; set; }
+    //public string? OneAbove { get; set; }
+    //public string? TwoAbove { get; set; }
+    //public string? ThreeAbove { get; set; }
+    //public string? BTS { get; set; }
+    //public string? OTS { get; set; }
+    //public string? YourPrediction { get; set; }
     public string? Href { get; set; }
     public string? RowBackground { get; set; }
     public MatchDetails Details { get; set; } // set after construction
@@ -56,7 +56,38 @@ public class TableDataItem
     
 
     public TableDataItem() { }
-
+    public TableDataItem(
+        string flag, string backgroundTipColour, string time,
+        string hostTeam, string? hostScore, string? guestScore, string guestTeam,
+        string? tip, long serverComputedLikes, string serverComputedLikesFormatted, int comments,
+        string? href, string rowBackground, List<TipAnalyzer.ProposedResult> proposedResults, bool isVipMatch,
+        string backgroundColor)
+    {
+        Flag = flag;
+        BackgroundTipColour = backgroundTipColour;
+        Time = time;
+        HostTeam = hostTeam;
+        HostScore = hostScore;
+        GuestScore = guestScore;
+        GuestTeam = guestTeam;
+        Tip = tip;
+        LikeButtonImage = likeButtonImage;
+        DislikeButtonImage = dislikeButtonImage;
+        LikePositive = likePositive;
+        LikeNegative = likeNegative;
+        ServerComputedLikes = serverComputedLikes;
+        ServerComputedLikesFormatted = serverComputedLikesFormatted;
+        Comments = comments;
+        One = coef1; Even = coef2; Two = coef3; HalfOne = coef4; HalfEven = coef5;
+        HalfTwo = coef6; OneAbove = coef7; TwoAbove = coef8; ThreeAbove = coef9; BTS = coef10; OTS = coef11;
+        //YourPrediction = version;
+        Href = href;
+        RowBackground = rowBackground;
+        ProposedResults = proposedResults;
+        IsVipMatch = isVipMatch;
+        BackgroundColor = backgroundColor;
+    }
+    /*
     public TableDataItem(
         string flag, string backgroundTipColour, string time,
         string hostTeam, string? hostScore, string? guestScore, string guestTeam,
@@ -91,4 +122,5 @@ public class TableDataItem
         IsVipMatch = isVipMatch;
         BackgroundColor = backgroundColor;
     }
+    */
 }
