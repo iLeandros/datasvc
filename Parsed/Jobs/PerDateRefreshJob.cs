edfadacb6b1bf565e5c8766fbe01896764c414b6
iLeandros <49075652+iLeandros@users.noreply.github.com>
2026-01-08 01:15:04 +0100

    Update PerDateRefreshJob.cs

--- Parsed/Jobs/PerDateRefreshJob.cs
+++ Parsed/Jobs/PerDateRefreshJob.cs
@@ -8,6 +8,17 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
+using DataSvc.Models;
+using DataSvc.ModelHelperCalls;
+using DataSvc.VIPHandler;
+using DataSvc.Auth; // AuthController + SessionAuthHandler namespace
+using DataSvc.MainHelpers; // MainHelpers
+using DataSvc.Likes; // MainHelpers
+using DataSvc.Services; // Services
+using DataSvc.Analyzer;
+using DataSvc.ClubElo;
+using DataSvc.MainHelpers;
+
 namespace DataSvc.Parsed;
 
 public sealed class PerDateRefreshJob : IHostedService, IDisposable
