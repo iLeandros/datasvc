c8e2f8de71cb5d086b73ea4f5b4fe8a277555176
iLeandros <49075652+iLeandros@users.noreply.github.com>
2026-01-08 01:14:34 +0100

    Update Endpoints.cs

--- Parsed/Endpoints.cs
+++ Parsed/Endpoints.cs
@@ -12,6 +12,15 @@ using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
+using DataSvc.Models;
+using DataSvc.ModelHelperCalls;
+using DataSvc.VIPHandler;
+using DataSvc.Auth; // AuthController + SessionAuthHandler namespace
+using DataSvc.MainHelpers; // MainHelpers
+using DataSvc.Likes; // MainHelpers
+using DataSvc.Services; // Services
+using DataSvc.Analyzer;
+using DataSvc.ClubElo;
 using DataSvc.MainHelpers;
 
 
