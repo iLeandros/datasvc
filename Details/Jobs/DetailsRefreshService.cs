f8dc54a21bd9fea24bf3f291af1aea669c581326
iLeandros <49075652+iLeandros@users.noreply.github.com>
2026-01-08 01:38:09 +0100

    Create DetailsRefreshService.cs

---
+++ Details/Jobs/DetailsRefreshService.cs
@@ -0,0 +1,17 @@
+// Details/DetailsRefreshService.cs
+using System;
+using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Logging;
+
+using DataSvc.Parsed;   // SnapshotPerDateStore
+// using Microsoft.Extensions.Configuration; // if you end up using it in the service
+
+namespace DataSvc.Details
+{
+    // paste the entire `public sealed class DetailsRefreshService { ... }` from Program.cs here
+}
